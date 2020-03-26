1398ab93e2ca19b74ed7c71fa98cd07ace74bfed
蔡金锋 <jeff.cai@99bill.com>
2020-03-26 13:37:30 +0800

    去掉多余方法

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 export { default as useAsync } from "./useAsync";
 export { default as useClientRect } from "./useClientRect";
 export { default as usePrevious } from "./usePrevious";
-export { default as useWindowTempVariable } from "./useWindowTempVariable";
 export { default as usePersistFn } from "./usePersistFn";
 export { default as useUpdateEffect } from "./useUpdateEffect";
