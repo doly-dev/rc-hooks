d125865cde45df6e6abab97cf9aaedec98fceddc
蔡金锋 <jeff.cai@99bill.com>
2021-11-09 14:49:43 +0800

    chore: eslint调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,39 @@
 // eslint-disable-next-line
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true,
-        "jest": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "react-hooks"
-    ],
-    "settings": {
-        react: {
-            version: "detect"
-        }
-    },
-    "rules": {
-        "@typescript-eslint/no-empty-interface": 0,
-        "@typescript-eslint/no-explicit-any": 0,
-        "@typescript-eslint/no-empty-function": 0,
-        "@typescript-eslint/explicit-module-boundary-types": 0,
-        "@typescript-eslint/ban-types": 0,
-        "react/prop-types": 0,
-        "react/display-name": 0,
-        "react-hooks/rules-of-hooks": "error", // 检查 Hook 的规则
-        "react-hooks/exhaustive-deps": "warn" // 检查 effect 的依赖
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
+  settings: {
+    react: {
+      version: 'detect'
     }
+  },
+  rules: {
+    '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/ban-types': 0,
+    'react/prop-types': 0,
+    'react/display-name': 0,
+    'react-hooks/rules-of-hooks': 'error', // 检查 Hook 的规则
+    'react-hooks/exhaustive-deps': 'warn' // 检查 effect 的依赖
+  }
 };
