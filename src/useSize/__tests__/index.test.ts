35a1f069349e0c63e73487b0d110274bf8f824d5
caijf <369756941@qq.com>
2022-08-31 10:47:10 +0800

    test: 补充 useSize 测试用例

--- src/useSize/__tests__/index.test.ts
+++ src/useSize/__tests__/index.test.ts
@@ -1,7 +1,45 @@
-import { useSize } from '../..';
+import { act, renderHook } from '@testing-library/react';
+import useSize from '..';
+
+let callback;
+jest.mock('../ResizeObserver', () => {
+  return jest.fn().mockImplementation((cb) => {
+    callback = cb;
+    return {
+      observe: () => { },
+      disconnect: () => { },
+    };
+  });
+});
 
 describe('useSize', () => {
   it('should be defined', () => {
     expect(useSize).toBeDefined();
   });
+
+  it('target is null', () => {
+    const { result } = renderHook(() => {
+      return useSize(null);
+    });
+
+    expect(result.current.height).toBeUndefined();
+    expect(result.current.width).toBeUndefined();
+  });
+
+  it('work', () => {
+    const targetEl = document.createElement('div');
+    const { result } = renderHook(() => useSize(targetEl));
+
+    act(() => {
+      callback([{
+        target: {
+          clientWidth: 100,
+          clientHeight: 50
+        }
+      }]);
+    });
+
+    expect(result.current).toEqual({ width: 100, height: 50 });
+  });
+
 });
