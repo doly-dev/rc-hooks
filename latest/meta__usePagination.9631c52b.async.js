89eca5d4377025c0efff31ae86cf59d2984e008e
caijf <caijf@users.noreply.github.com>
2025-03-27 09:26:15 +0000

    deploy: da7dc1171fd9c2bae2b1f06c70e2baa11938b2fb

---
+++ latest/meta__usePagination.9631c52b.async.js
@@ -0,0 +1,286 @@
+"use strict";(self.webpackChunkrc_hooks=self.webpackChunkrc_hooks||[]).push([[5730],{51893:function(c,t,n){var r;n.r(t),n.d(t,{demos:function(){return x}});var _=n(90228),d=n.n(_),p=n(87999),v=n.n(p),a=n(75271),h=n(72216),f=n(39508),u=n(30554),I=n(90674),P=n.n(I),g=n(9670),x={"src-use-pagination-demo-pagination1":{component:a.memo(a.lazy(function(){return Promise.all([n.e(5173),n.e(1918),n.e(9046),n.e(8996),n.e(2433)]).then(n.bind(n,15597))})),asset:{type:"BLOCK",id:"src-use-pagination-demo-pagination1",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(39245).Z},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.24.5"},"rc-hooks":{type:"NPM",value:"3.0.18"},"./services/getUserList.ts":{type:"FILE",value:n(19253).Z},mockjs:{type:"NPM",value:"1.1.0"},ut2:{type:"NPM",value:"1.17.0"}},entry:"index.tsx"},context:{"./services/getUserList.ts":u,react:r||(r=n.t(a,2)),antd:h,"rc-hooks":f,"/home/runner/work/rc-hooks/rc-hooks/src/usePagination/demos/services/getUserList.ts":u,mockjs:I,ut2:g},renderOpts:{compile:function(){var m=v()(d()().mark(function i(){var l,o=arguments;return d()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(1948).then(n.bind(n,81948));case 2:return e.abrupt("return",(l=e.sent).default.apply(l,o));case 3:case"end":return e.stop()}},i)}));function s(){return m.apply(this,arguments)}return s}()}},"src-use-pagination-demo-pagination2":{component:a.memo(a.lazy(function(){return Promise.all([n.e(5173),n.e(1918),n.e(9046),n.e(8996),n.e(2433)]).then(n.bind(n,17795))})),asset:{type:"BLOCK",id:"src-use-pagination-demo-pagination2",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(29009).Z},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.24.5"},"rc-hooks":{type:"NPM",value:"3.0.18"},"./services/getUserList.ts":{type:"FILE",value:n(19253).Z},mockjs:{type:"NPM",value:"1.1.0"},ut2:{type:"NPM",value:"1.17.0"}},entry:"index.tsx",title:"\u67E5\u8BE2\u8868\u5355\u548CAnt Table"},context:{"./services/getUserList.ts":u,react:r||(r=n.t(a,2)),antd:h,"rc-hooks":f,"/home/runner/work/rc-hooks/rc-hooks/src/usePagination/demos/services/getUserList.ts":u,mockjs:I,ut2:g},renderOpts:{compile:function(){var m=v()(d()().mark(function i(){var l,o=arguments;return d()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(1948).then(n.bind(n,81948));case 2:return e.abrupt("return",(l=e.sent).default.apply(l,o));case 3:case"end":return e.stop()}},i)}));function s(){return m.apply(this,arguments)}return s}()}},"src-use-pagination-demo-pagination3":{component:a.memo(a.lazy(function(){return Promise.all([n.e(5173),n.e(1918),n.e(9046),n.e(8996),n.e(2433)]).then(n.bind(n,53999))})),asset:{type:"BLOCK",id:"src-use-pagination-demo-pagination3",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(7987).Z},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.24.5"},"rc-hooks":{type:"NPM",value:"3.0.18"},"./services/getUserList.ts":{type:"FILE",value:n(19253).Z},mockjs:{type:"NPM",value:"1.1.0"},ut2:{type:"NPM",value:"1.17.0"}},entry:"index.tsx",title:"Ant Table"},context:{"./services/getUserList.ts":u,react:r||(r=n.t(a,2)),antd:h,"rc-hooks":f,"/home/runner/work/rc-hooks/rc-hooks/src/usePagination/demos/services/getUserList.ts":u,mockjs:I,ut2:g},renderOpts:{compile:function(){var m=v()(d()().mark(function i(){var l,o=arguments;return d()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(1948).then(n.bind(n,81948));case 2:return e.abrupt("return",(l=e.sent).default.apply(l,o));case 3:case"end":return e.stop()}},i)}));function s(){return m.apply(this,arguments)}return s}()}}}},30554:function(c,t,n){n.r(t),n.d(t,{default:function(){return I}});var r=n(90228),_=n.n(r),d=n(82092),p=n.n(d),v=n(87999),a=n.n(v),h=n(90674),f=n.n(h),u=n(67424);function I(g){return P.apply(this,arguments)}function P(){return P=a()(_()().mark(function g(x){var m,s,i;return _()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return m=x.current,s=x.pageSize,i=s===void 0?5:s,o.next=3,(0,u.Z)();case 3:return o.abrupt("return",f().mock(p()(p()(p()(p()({},"data|".concat(i),[{id:"@guid",name:"@cname","gender|1":["male","female"],email:"@email",disabled:!1}]),"total",15),"errCode","00"),"errMsg","")));case 4:case"end":return o.stop()}},g)})),P.apply(this,arguments)}},18891:function(c,t,n){n.r(t),n.d(t,{texts:function(){return r}});const r=[{value:"\u57FA\u4E8E ",paraId:0,tocIndex:0},{value:"useAsync",paraId:1,tocIndex:0},{value:" \u6269\u5C55\uFF0C\u7528\u4E8E\u7BA1\u7406",paraId:0,tocIndex:0},{value:"\u5206\u9875",paraId:0,tocIndex:0},{value:"\u7684 Hook\u3002",paraId:0,tocIndex:0},{value:"\u6838\u5FC3\u7279\u6027",paraId:2,tocIndex:0},{value:"\u81EA\u52A8\u7BA1\u7406\u5206\u9875 ",paraId:3,tocIndex:0},{value:"current",paraId:3,tocIndex:0},{value:" ",paraId:3,tocIndex:0},{value:"pageSize",paraId:3,tocIndex:0},{value:"asyncFn",paraId:3,tocIndex:0},{value:" \u7B2C\u4E00\u4E2A\u53C2\u6570\u5FC5\u5B9A\u542B\u6709 ",paraId:3,tocIndex:0},{value:"{ current, pageSize }",paraId:3,tocIndex:0},{value:"asyncFn",paraId:3,tocIndex:0},{value:" \u8FD4\u56DE\u7684\u6570\u636E\u7ED3\u6784\u5FC5\u987B\u5305\u542B ",paraId:3,tocIndex:0},{value:"{ list: DataItem[], total: number; }",paraId:3,tocIndex:0},{value:" \u3002",paraId:3,tocIndex:0},{value:"\u989D\u5916\u8FD4\u56DE ",paraId:3,tocIndex:0},{value:"pagination",paraId:3,tocIndex:0},{value:" \u5B57\u6BB5\uFF0C\u5305\u542B\u6240\u6709\u5206\u9875\u4FE1\u606F\uFF0C\u53CA\u64CD\u4F5C\u5206\u9875\u7684\u51FD\u6570\u3002",paraId:3,tocIndex:0},{value:"\u989D\u5916\u8FD4\u56DE ",paraId:3,tocIndex:0},{value:"tableProps",paraId:3,tocIndex:0},{value:" \u5B57\u6BB5\uFF0C\u9002\u914D ",paraId:3,tocIndex:0},{value:"antd Table",paraId:3,tocIndex:0},{value:" \u7EC4\u4EF6\u7684\u6570\u636E\u7ED3\u6784\uFF0C\u53EF\u4EE5\u76F4\u63A5\u7528\u5728 ",paraId:3,tocIndex:0},{value:"antd Table",paraId:3,tocIndex:0},{value:" \u7EC4\u4EF6\u4E0A\u3002\u3002",paraId:3,tocIndex:0},{value:"refreshDeps",paraId:3,tocIndex:0},{value:" \u53D8\u5316\u540E\uFF0C\u4F1A\u91CD\u7F6E",paraId:3,tocIndex:0},{value:"current",paraId:3,tocIndex:0},{value:"\u4E3A 1\uFF0C\u5E76\u89E6\u53D1\u5F02\u6B65\u65B9\u6CD5\u3002",paraId:3,tocIndex:0},{value:`const {
+  ...,
+  pagination: {
+    current: number;
+    pageSize: number;
+    total: number;
+    totalPage: number;
+    onChange: (current: number, pageSize: number) => void;
+    changeCurrent: (current: number) => void;
+    changePageSize: (pageSize: number) => void;
+  };
+  tableProps: {
+    dataSource: DataItem[];
+    loading: boolean;
+    onChange: (
+      pagination: any,
+      filters?: any,
+      sorter?: any,
+    ) => void;
+    pagination: {
+      current: number;
+      pageSize: number;
+      total: number;
+    };
+  };
+} = usePagination(async ({ current, data, sorter?, filters?, extra? }) => ({
+    list,
+    total,
+    ...
+  }), {
+  ...,
+  defaultPageSize,
+  refreshDeps,
+});
+`,paraId:4,tocIndex:7},{value:"\u9664\u4E86\u4EE5\u4E0B\uFF0C\u5176\u5B83\u548C ",paraId:5,tocIndex:7},{value:"useAsync",paraId:6,tocIndex:7},{value:" \u4E00\u6837\u3002",paraId:5,tocIndex:7},{value:"\u53C2\u6570",paraId:7,tocIndex:8},{value:"\u8BF4\u660E",paraId:7,tocIndex:8},{value:"\u7C7B\u578B",paraId:7,tocIndex:8},{value:"pagination",paraId:7,tocIndex:8},{value:"\u5206\u9875\u6570\u636E\u53CA\u64CD\u4F5C\u5206\u9875\u7684\u65B9\u6CD5\u3002",paraId:7,tocIndex:8},{value:"-",paraId:7,tocIndex:8},{value:"tableProps",paraId:7,tocIndex:8},{value:"\u9002\u914D antd Table \u7EC4\u4EF6\u7684\u6570\u636E\u7ED3\u6784\uFF0C\u53EF\u4EE5\u76F4\u63A5\u7528\u5728 antd Table \u7EC4\u4EF6\u4E0A\u3002",paraId:7,tocIndex:8},{value:"-",paraId:7,tocIndex:8},{value:"\u53C2\u6570",paraId:8,tocIndex:9},{value:"\u8BF4\u660E",paraId:8,tocIndex:9},{value:"\u7C7B\u578B",paraId:8,tocIndex:9},{value:"\u9ED8\u8BA4\u503C",paraId:8,tocIndex:9},{value:"defaultPageSize",paraId:8,tocIndex:9},{value:"\u9ED8\u8BA4\u6BCF\u9875\u7684\u6570\u91CF\u3002",paraId:8,tocIndex:9},{value:"number",paraId:8,tocIndex:9},{value:"10",paraId:8,tocIndex:9},{value:"refreshDeps",paraId:8,tocIndex:9},{value:"\u5728 ",paraId:8,tocIndex:9},{value:"autoRun = true",paraId:8,tocIndex:9},{value:" \u65F6\uFF0CrefreshDeps \u53D8\u5316\uFF0C\u4F1A\u91CD\u7F6E ",paraId:8,tocIndex:9},{value:"current",paraId:8,tocIndex:9},{value:" \u5230\u7B2C\u4E00\u9875\uFF0C\u5E76\u6267\u884C ",paraId:8,tocIndex:9},{value:"run",paraId:8,tocIndex:9},{value:" \u65B9\u6CD5\u3002",paraId:8,tocIndex:9},{value:"any[]",paraId:8,tocIndex:9},{value:"-",paraId:8,tocIndex:9}]},39245:function(c,t){t.Z=`import React from 'react';
+import { List, Pagination } from 'antd';
+import { usePagination } from 'rc-hooks';
+import getUserList from './services/getUserList';
+
+function Demo() {
+  const { data, loading, pagination } = usePagination(({ current, pageSize }) => {
+    console.log(current, pageSize);
+    return getUserList({ current, pageSize }).then((res) => ({
+      list: res.data,
+      total: res.total
+    }));
+  });
+  console.log(pagination);
+  return (
+    <div>
+      <List
+        dataSource={data?.list || []}
+        loading={loading}
+        renderItem={(item: { name: string; email: string }) => (
+          <List.Item>
+            {item.name} - {item.email}
+          </List.Item>
+        )}
+      />
+      <Pagination
+        current={pagination.current}
+        pageSize={pagination.pageSize}
+        total={pagination.total}
+        onChange={pagination.onChange}
+        onShowSizeChange={pagination.onChange}
+        showQuickJumper
+        showSizeChanger
+        showTotal={(total) => \`\u5171 \${total} \u6761\u6570\u636E\`}
+        style={{ marginTop: 16, textAlign: 'right' }}
+      />
+    </div>
+  );
+}
+
+export default Demo;
+`},29009:function(c,t){t.Z=`import React, { useCallback, useEffect } from 'react';
+import { Form, Input, Select, Button, Table } from 'antd';
+import { usePagination } from 'rc-hooks';
+import getUserList from './services/getUserList';
+
+const formItemLayout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 14 }
+};
+
+const buttonItemLayout = {
+  wrapperCol: { offset: 4, span: 14 }
+};
+
+const columns = [
+  {
+    title: 'name',
+    dataIndex: 'name'
+  },
+  {
+    title: 'email',
+    dataIndex: 'email'
+  },
+  {
+    title: 'id',
+    dataIndex: 'id',
+    sorter: true
+  },
+  {
+    title: 'gender',
+    dataIndex: 'gender'
+  }
+];
+
+function Demo() {
+  const [form] = Form.useForm();
+  const { run, params, refresh, loading, pagination, tableProps } = usePagination(
+    ({ current, pageSize, ...rest }) => {
+      console.log({ current, pageSize, ...rest });
+      return getUserList({ current, pageSize }).then((res) => ({
+        list: res.data,
+        total: res.total
+      }));
+    },
+    {
+      autoRun: false,
+      defaultPageSize: 5
+    }
+  );
+
+  const handleReset = useCallback(() => {
+    form.resetFields();
+    form.submit();
+  }, [form]);
+
+  useEffect(() => {
+    form.submit();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      <Form
+        {...formItemLayout}
+        form={form}
+        initialValues={{}}
+        onFinish={(values) => {
+          run({
+            ...params[0],
+            current: 1,
+            pageSize: pagination.pageSize,
+            search: values
+          });
+        }}
+      >
+        <Form.Item label="name" name="name">
+          <Input placeholder="\u8BF7\u8F93\u5165" />
+        </Form.Item>
+        <Form.Item label="gender" name="gender">
+          <Select placeholder="\u8BF7\u9009\u62E9">
+            <Select.Option value="male">male</Select.Option>
+            <Select.Option value="female">female</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item {...buttonItemLayout}>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            \u67E5\u8BE2
+          </Button>
+          <Button onClick={handleReset} style={{ marginLeft: 16 }} disabled={loading}>
+            \u91CD\u7F6E
+          </Button>
+          <Button onClick={refresh} style={{ marginLeft: 16 }} disabled={loading}>
+            \u5237\u65B0
+          </Button>
+        </Form.Item>
+      </Form>
+      <Table
+        {...tableProps}
+        pagination={{ ...tableProps.pagination, showSizeChanger: true, showQuickJumper: true }}
+        columns={columns}
+        rowKey="id"
+        bordered
+      />
+    </div>
+  );
+}
+
+export default Demo;
+`},7987:function(c,t){t.Z=`import React from 'react';
+import { Button, Table } from 'antd';
+import { usePagination } from 'rc-hooks';
+import getUserList from './services/getUserList';
+
+function Demo() {
+  const { refresh, tableProps } = usePagination(
+    ({ current, pageSize, ...rest }) => {
+      console.log(rest);
+      return getUserList({ current, pageSize }).then((res) => ({
+        list: res.data,
+        total: res.total
+      }));
+    },
+    {
+      // autoRun: false
+    }
+  );
+
+  const columns = [
+    {
+      title: 'name',
+      dataIndex: 'name'
+    },
+    {
+      title: 'email',
+      dataIndex: 'email'
+    },
+    {
+      title: 'id',
+      dataIndex: 'id',
+      sorter: true
+    },
+    {
+      title: 'gender',
+      dataIndex: 'gender',
+      filters: [
+        {
+          text: 'male',
+          value: 'male'
+        },
+        {
+          text: 'female',
+          value: 'female'
+        }
+      ]
+    }
+  ];
+
+  return (
+    <div>
+      <Button onClick={refresh} style={{ marginBottom: 16 }}>
+        \u5237\u65B0
+      </Button>
+      <Table {...tableProps} columns={columns} rowKey="id" bordered />
+    </div>
+  );
+}
+
+export default Demo;
+`},19253:function(c,t){t.Z=`/* eslint-disable @typescript-eslint/no-unused-vars */
+import Mock from 'mockjs';
+import { sleep } from 'ut2';
+
+type DataItem = {
+  id: string;
+  name: string;
+  gender: 'male' | 'female';
+  email: string;
+  disabled: boolean;
+};
+
+type Result = {
+  data: DataItem[];
+  total: number;
+  errCode: string;
+  errMsg: string;
+};
+
+export default async function getUserList({
+  current,
+  pageSize = 5
+}: {
+  current: number;
+  pageSize?: number;
+}) {
+  await sleep();
+  return Mock.mock({
+    [\`data|\${pageSize}\`]: [
+      {
+        id: '@guid',
+        name: '@cname',
+        'gender|1': ['male', 'female'],
+        email: '@email',
+        disabled: false
+      }
+    ],
+    total: 15,
+    errCode: '00',
+    errMsg: ''
+  }) as Result;
+}
+`}}]);
