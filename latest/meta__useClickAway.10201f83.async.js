636f77e44622c81f663faafbe20329c2d302d6ca
caijf <caijf@users.noreply.github.com>
2025-09-09 09:35:00 +0000

    deploy: 7ffd010604457ff004460d44b5db09efa8bc03e0

---
+++ latest/meta__useClickAway.10201f83.async.js
@@ -0,0 +1,128 @@
+"use strict";(self.webpackChunkrc_hooks=self.webpackChunkrc_hooks||[]).push([[4568],{44443:function(m,t,n){var a;n.r(t),n.d(t,{demos:function(){return h}});var v=n(90228),r=n.n(v),y=n(87999),i=n.n(y),o=n(75271),p=n(51953),h={"src-use-click-away-demo-basic":{component:o.memo(o.lazy(function(){return Promise.all([n.e(8883),n.e(6586),n.e(3484),n.e(5845),n.e(2433)]).then(n.bind(n,55476))})),asset:{type:"BLOCK",id:"src-use-click-away-demo-basic",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(33303).Z},react:{type:"NPM",value:"18.3.1"},"rc-hooks":{type:"NPM",value:"3.1.0"}},entry:"index.tsx"},context:{react:a||(a=n.t(o,2)),"rc-hooks":p},renderOpts:{compile:function(){var u=i()(r()().mark(function c(){var s,d=arguments;return r()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,Promise.all([n.e(1620),n.e(542)]).then(n.bind(n,10542));case 2:return e.abrupt("return",(s=e.sent).default.apply(s,d));case 3:case"end":return e.stop()}},c)}));function l(){return u.apply(this,arguments)}return l}()}},"src-use-click-away-demo-definedom":{component:o.memo(o.lazy(function(){return Promise.all([n.e(8883),n.e(6586),n.e(3484),n.e(5845),n.e(2433)]).then(n.bind(n,71480))})),asset:{type:"BLOCK",id:"src-use-click-away-demo-definedom",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(4410).Z},react:{type:"NPM",value:"18.3.1"},"rc-hooks":{type:"NPM",value:"3.1.0"}},entry:"index.tsx"},context:{react:a||(a=n.t(o,2)),"rc-hooks":p},renderOpts:{compile:function(){var u=i()(r()().mark(function c(){var s,d=arguments;return r()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,Promise.all([n.e(1620),n.e(542)]).then(n.bind(n,10542));case 2:return e.abrupt("return",(s=e.sent).default.apply(s,d));case 3:case"end":return e.stop()}},c)}));function l(){return u.apply(this,arguments)}return l}()}},"src-use-click-away-demo-moredom":{component:o.memo(o.lazy(function(){return Promise.all([n.e(8883),n.e(6586),n.e(3484),n.e(5845),n.e(2433)]).then(n.bind(n,92744))})),asset:{type:"BLOCK",id:"src-use-click-away-demo-moredom",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(83482).Z},react:{type:"NPM",value:"18.3.1"},"rc-hooks":{type:"NPM",value:"3.1.0"}},entry:"index.tsx"},context:{react:a||(a=n.t(o,2)),"rc-hooks":p},renderOpts:{compile:function(){var u=i()(r()().mark(function c(){var s,d=arguments;return r()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,Promise.all([n.e(1620),n.e(542)]).then(n.bind(n,10542));case 2:return e.abrupt("return",(s=e.sent).default.apply(s,d));case 3:case"end":return e.stop()}},c)}));function l(){return u.apply(this,arguments)}return l}()}},"src-use-click-away-demo-defineevent":{component:o.memo(o.lazy(function(){return Promise.all([n.e(8883),n.e(6586),n.e(3484),n.e(5845),n.e(2433)]).then(n.bind(n,73008))})),asset:{type:"BLOCK",id:"src-use-click-away-demo-defineevent",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(97868).Z},react:{type:"NPM",value:"18.3.1"},"rc-hooks":{type:"NPM",value:"3.1.0"}},entry:"index.tsx"},context:{react:a||(a=n.t(o,2)),"rc-hooks":p},renderOpts:{compile:function(){var u=i()(r()().mark(function c(){var s,d=arguments;return r()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,Promise.all([n.e(1620),n.e(542)]).then(n.bind(n,10542));case 2:return e.abrupt("return",(s=e.sent).default.apply(s,d));case 3:case"end":return e.stop()}},c)}));function l(){return u.apply(this,arguments)}return l}()}},"src-use-click-away-demo-moreevent":{component:o.memo(o.lazy(function(){return Promise.all([n.e(8883),n.e(6586),n.e(3484),n.e(5845),n.e(2433)]).then(n.bind(n,37375))})),asset:{type:"BLOCK",id:"src-use-click-away-demo-moreevent",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(5714).Z},react:{type:"NPM",value:"18.3.1"},"rc-hooks":{type:"NPM",value:"3.1.0"}},entry:"index.tsx"},context:{react:a||(a=n.t(o,2)),"rc-hooks":p},renderOpts:{compile:function(){var u=i()(r()().mark(function c(){var s,d=arguments;return r()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,Promise.all([n.e(1620),n.e(542)]).then(n.bind(n,10542));case 2:return e.abrupt("return",(s=e.sent).default.apply(s,d));case 3:case"end":return e.stop()}},c)}));function l(){return u.apply(this,arguments)}return l}()}}}},99200:function(m,t,n){n.r(t),n.d(t,{texts:function(){return a}});const a=[{value:"\u7BA1\u7406\u76EE\u6807\u5143\u7D20\u5916\u4E8B\u4EF6\u7684 Hook\u3002",paraId:0,tocIndex:0},{value:"\u53C2\u6570",paraId:1,tocIndex:7},{value:"\u8BF4\u660E",paraId:1,tocIndex:7},{value:"\u7C7B\u578B",paraId:1,tocIndex:7},{value:"\u9ED8\u8BA4\u503C",paraId:1,tocIndex:7},{value:"ref",paraId:1,tocIndex:7},{value:"Dom \u8282\u70B9\u6216 Ref \u5BF9\u8C61\uFF0C\u652F\u6301\u6570\u7EC4\u3002",paraId:1,tocIndex:7},{value:"RefType | RefType[]",paraId:1,tocIndex:7},{value:"-",paraId:1,tocIndex:7},{value:"onClickAway",paraId:1,tocIndex:7},{value:"\u89E6\u53D1\u4E8B\u4EF6\u7684\u51FD\u6570\u3002",paraId:1,tocIndex:7},{value:"(event)=>void",paraId:1,tocIndex:7},{value:"-",paraId:1,tocIndex:7},{value:"events",paraId:1,tocIndex:7},{value:"\u76D1\u542C\u4E8B\u4EF6\u540D\u79F0\uFF0C\u652F\u6301\u6570\u7EC4\u3002",paraId:1,tocIndex:7},{value:"string | string[]",paraId:1,tocIndex:7},{value:"click",paraId:1,tocIndex:7},{value:`export type RefType<T = HTMLElement> =
+  | RefObject<T>
+  | null
+  | (() => T | null)
+  | T
+  | MutableRefObject<T | undefined>;
+`,paraId:2,tocIndex:7}]},33303:function(m,t){t.Z=`import * as React from 'react';
+import { useClickAway } from 'rc-hooks';
+
+const Demo: React.FC = () => {
+  const [count, setCount] = React.useState(0);
+  const ref = React.useRef<HTMLSpanElement>(null);
+
+  useClickAway(ref, () => {
+    setCount((c) => c + 1);
+  });
+
+  return (
+    <div>
+      <span ref={ref}>
+        <button>box</button>
+      </span>
+      <p>count: {count}</p>
+    </div>
+  );
+};
+
+export default Demo;
+`},4410:function(m,t){t.Z=`import * as React from 'react';
+import { useClickAway } from 'rc-hooks';
+
+const Demo: React.FC = () => {
+  const [count, setCount] = React.useState(0);
+
+  useClickAway(
+    () => document.querySelector('#box'),
+    () => {
+      setCount((c) => c + 1);
+    }
+  );
+
+  return (
+    <div>
+      <span id="box">
+        <button>box</button>
+      </span>
+      <p>count: {count}</p>
+    </div>
+  );
+};
+
+export default Demo;
+`},97868:function(m,t){t.Z=`import * as React from 'react';
+import { useClickAway } from 'rc-hooks';
+
+const Demo: React.FC = () => {
+  const [count, setCount] = React.useState(0);
+
+  useClickAway(
+    () => document.querySelector('#box3'),
+    () => {
+      setCount((c) => c + 1);
+    },
+    'contextmenu'
+  );
+
+  return (
+    <div>
+      <span id="box3">
+        <button>box</button>
+      </span>
+      <p>count: {count}</p>
+    </div>
+  );
+};
+
+export default Demo;
+`},83482:function(m,t){t.Z=`import * as React from 'react';
+import { useClickAway } from 'rc-hooks';
+
+const Demo: React.FC = () => {
+  const [count, setCount] = React.useState(0);
+  const ref = React.useRef<HTMLSpanElement>(null);
+
+  useClickAway([ref, () => document.querySelector('#box2')], () => {
+    setCount((c) => c + 1);
+  });
+
+  return (
+    <div>
+      <span ref={ref}>
+        <button>box</button>
+      </span>
+      <span id="box2" style={{ marginLeft: 20 }}>
+        <button>box</button>
+      </span>
+      <p>count: {count}</p>
+    </div>
+  );
+};
+
+export default Demo;
+`},5714:function(m,t){t.Z=`import * as React from 'react';
+import { useClickAway } from 'rc-hooks';
+
+const Demo: React.FC = () => {
+  const [count, setCount] = React.useState(0);
+
+  useClickAway(
+    () => document.querySelector('#box4'),
+    () => {
+      setCount((c) => c + 1);
+    },
+    ['contextmenu', 'click']
+  );
+
+  return (
+    <div>
+      <span id="box4">
+        <button>box</button>
+      </span>
+      <p>count: {count}</p>
+    </div>
+  );
+};
+
+export default Demo;
+`}}]);
