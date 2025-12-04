8486b9b9e7094494740d317383f3d40e2897daa1
caijf <caijf@users.noreply.github.com>
2025-12-04 02:09:20 +0000

    deploy: 2562ef781e0a949cca68ad6f85b3ebe4824a9f53

---
+++ latest/meta__useLatest.ea223746.async.js
@@ -0,0 +1,33 @@
+"use strict";(self.webpackChunkrc_hooks=self.webpackChunkrc_hooks||[]).push([[3254],{64034:function(r,t,n){var o;n.r(t),n.d(t,{demos:function(){return h}});var l=n(90228),a=n.n(l),m=n(87999),c=n.n(m),s=n(75271),d=n(14962),_=n(52620),h={"src-use-latest-demo-basic":{component:s.memo(s.lazy(function(){return Promise.all([n.e(8883),n.e(2869),n.e(8941),n.e(5845),n.e(2433)]).then(n.bind(n,69727))})),asset:{type:"BLOCK",id:"src-use-latest-demo-basic",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(22341).Z},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.29.1"},"rc-hooks":{type:"NPM",value:"3.2.0"}},entry:"index.tsx"},context:{react:o||(o=n.t(s,2)),antd:d,"rc-hooks":_},renderOpts:{compile:function(){var i=c()(a()().mark(function E(){var u,f=arguments;return a()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,Promise.all([n.e(1620),n.e(9250)]).then(n.bind(n,29250));case 2:return e.abrupt("return",(u=e.sent).default.apply(u,f));case 3:case"end":return e.stop()}},E)}));function p(){return i.apply(this,arguments)}return p}()}}}},94875:function(r,t,n){n.r(t),n.d(t,{texts:function(){return o}});const o=[{value:"\u8FD4\u56DE\u6700\u65B0\u7684 state \u6216 props \u3002",paraId:0,tocIndex:0},{value:`const latestValue = useLatest(value);
+`,paraId:1,tocIndex:2}]},22341:function(r,t){t.Z=`import * as React from 'react';
+import { Space, Button } from 'antd';
+import { useLatest } from 'rc-hooks';
+
+function Demo() {
+  const [count, setCount] = React.useState(0);
+  const latestCount = useLatest(count);
+
+  const timer = React.useRef<any>(null);
+
+  const handleAlertClick = () => {
+    clearTimeout(timer.current);
+    timer.current = setTimeout(() => {
+      alert(\`\u6700\u65B0\u7684 count \u503C\u4E3A\uFF1A\${latestCount.current}\`);
+    }, 3000);
+  };
+
+  return (
+    <div>
+      <p>\u70B9\u51FB {count} \u6B21</p>
+      <Space>
+        <Button type="primary" onClick={() => setCount(count + 1)}>
+          \u70B9\u6211
+        </Button>
+        <Button onClick={handleAlertClick}>\u70B9\u51FB3\u79D2\u540E\u5F39\u7A97\u663E\u793A\u70B9\u51FB\u6B21\u6570</Button>
+      </Space>
+    </div>
+  );
+}
+
+export default Demo;
+`}}]);
