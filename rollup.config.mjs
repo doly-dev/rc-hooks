7d8a13d704633afaf02a6c6802ef4cfa3dfa4351
caijf <369756941@qq.com>
2026-01-28 16:50:26 +0800

    build: 构建配置调整，影响打包后的目录结构

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,29 +5,62 @@ import terser from '@rollup/plugin-terser';
 
 const globalVarName = 'rcHooks';
 
-export default {
-  input: './src/index.ts',
-  external: ['react'],
-  output: [
-    {
-      file: `dist/${globalVarName}.js`,
-      format: 'umd',
-      name: globalVarName,
-      globals: {
-        react: 'React'
-      },
-      sourcemap: true
-    },
-    {
-      file: `dist/${globalVarName}.min.js`,
-      format: 'umd',
-      name: globalVarName,
-      globals: {
-        react: 'React'
-      },
-      sourcemap: true,
-      plugins: [terser()]
-    }
-  ],
-  plugins: [resolve(), commonjs(), typescript({ tsconfig: 'tsconfig.build.json' })]
+const commonConfig = {
+  input: 'src/index.ts',
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.build.json',
+      removeComments: true
+    })
+  ]
 };
+
+export default [
+  {
+    ...commonConfig,
+    output: [
+      {
+        format: 'cjs',
+        dir: 'lib',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        exports: 'named'
+      },
+      {
+        format: 'es',
+        dir: 'es',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        exports: 'named'
+      }
+    ],
+    external: ['react', 'tslib', 'ut2', 'util-helpers', 'cache2']
+  },
+  {
+    ...commonConfig,
+    external: ['react'],
+    output: [
+      {
+        file: `dist/${globalVarName}.js`,
+        format: 'umd',
+        name: globalVarName,
+        globals: {
+          react: 'React'
+        },
+        sourcemap: true
+      },
+      {
+        file: `dist/${globalVarName}.min.js`,
+        format: 'umd',
+        name: globalVarName,
+        globals: {
+          react: 'React'
+        },
+        sourcemap: true,
+        plugins: [terser()]
+      }
+    ]
+  }
+];
